ae8ebc02ea6702f02f9b7b69c7468ea4508137be
Liam <liam.baranzo@gmail.com>
2014-12-16 10:37:32 +0000

    Revert "Revert "Modifications made but with Error""
    This reverts commit f46a2f4905df8abcd082d6e7b2978b67de4666da.

--- Cerberus/GameObject.cpp
+++ Cerberus/GameObject.cpp
@@ -203,16 +203,16 @@ GameObject * GameObject::getChild(int index)
 		return NULL;
 }
 
-Component * GameObject::getComponentByType(const std::string& type)
-{
-	Component *found = NULL;
-	//std::find_if(m_Components.begin(),m_Components.end(),[type](Component &n){return n.getType()==type;});
-	return found;
-}
+//Component * GameObject::getComponentByType(const std::string& type)
+//{
+//	Component *found = NULL;
+//	//std::find_if(m_Components.begin(),m_Components.end(),[type](Component &n){return n.getType()==type;});
+//	return found;
+//}
 
-Component * GameObject::getComponentByName(const std::string& name)
-{
-	Component *found = NULL;
-	//std::find_if(m_Components.begin(),m_Components.end(),[type](Component &n){return n.getType()==type;});
-	return found;
-}
+//Component * GameObject::getComponentByName(const std::string& name)
+//{
+//	Component *found = NULL;
+//	//std::find_if(m_Components.begin(),m_Components.end(),[type](Component &n){return n.getType()==type;});
+//	return found;
+//}
