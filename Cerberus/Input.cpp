7c453f46096a32a98dc5bb3aaa38413ee5defeec
sinnlaw <chinglonglaw@gmail.com>
2014-12-18 20:04:57 +0900

    SkyFrame
    This is the framework with the skybox

---
+++ Cerberus/Input.cpp
@@ -0,0 +1,47 @@
+//Laim and Chris CameraControl
+
+#include "Input.h"
+
+Input::Input()
+{
+	m_Mouse = NULL;
+	m_Keyboard = NULL;
+
+}
+
+Input::~Input()
+{
+
+}
+
+void Input::destroy()
+{
+
+	if (m_Mouse)
+	{
+		delete m_Mouse;
+		m_Mouse = NULL;
+	}
+	if (m_Keyboard)
+	{
+		delete m_Keyboard;
+		m_Keyboard = NULL;
+	}
+}
+
+void Input::update()
+{
+	m_Keyboard->update();
+}
+
+bool Input::init()
+{
+	m_Keyboard = new Keyboard();
+	m_Mouse = new Mouse();
+	//SDL_GameControllerAddMappingsFromFile(inputDBFilename.c_str());
+
+	return true;
+}
+
+//to be put in or removed from last function
+/*const std::string& inputDBFilename*/
